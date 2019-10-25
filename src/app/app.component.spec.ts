66377eea2110df41912e267eeeff0d0915cce336
mdelarosa90 <michel.delarosa@pluriza.com>
2019-10-25 14:56:05 -0500

    Testing Router Outlet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        UsersComponent,
+        UserDetailsComponent
       ],
+      imports: [RouterTestingModule.withRoutes(routes)]
     }).compileComponents();
   }));
 
@@ -22,10 +33,20 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('awesomeApp');
   });
 
-  it('should render title', () => {
+  it('should have a router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('awesomeApp app is running!');
+    const de = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(de).not.toBeNull();
   });
+
+  it ('should have a link to users page', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+
+    const index = debugElements.findIndex(de => de.attributes['routerLink'] === 'users');
+    expect(index).toBeGreaterThan(-1);
+  });
+
 });
